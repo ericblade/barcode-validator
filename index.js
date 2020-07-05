2890d911ac0e6143ca528865adb94a580f95a829
Eric Blade <blade.eric@gmail.com>
2020-07-05 07:43:23 -0400

    add some amazon asin support

--- index.js
+++ index.js
@@ -91,13 +91,26 @@ const validIsbn13Checksum = (isbn) => {
     return getIsbn13Checksum(isbn) == check;
 }
 
+// https://stackoverflow.com/questions/2123131/determine-if-10-digit-string-is-valid-amazon-asin
+// effectively tells us "does this string start with a "B" and is 10 alpha-numeric digits.
+// Unfortunately, 10 digit strings such as BOOTSTRAPS look like a perfectly valid ASIN, and you wouldn't
+// know the difference without asking Amazon if it's real.
+
+const validAsin = (asin) => {
+    return /^(B[\dA-Z]{9}|\d{9}(X|\d))$/.test(asin);
+}
+
 function getTypeOfBarcode(code) {
-    console.warn('*getTypeOfBarcode', code, typeof code);
     switch(code.length) {
-        case 10: return 'isbn10';
+        case 10: {
+            if (code.startsWith('B')) {
+                return 'asin';
+            }
+            return 'isbn10';
+        }
         case 12: return 'upc';
         case 13: {
-            if(/^(978|979|290|291)[0-9].*/.match(code)) {
+            if(/^(978|979|290|291)[0-9].*/.test(code)) {
                 return 'isbn13';
             }
             return 'gtin';
@@ -111,6 +124,7 @@ const validatorMap = {
     upc: validUpcChecksum,
     isbn10: validIsbn10Checksum,
     isbn13: validIsbn13Checksum,
+    asin: validAsin,
 };
 
 // Given a barcode and an optional type, will attempt to correct an invalid code in some possible ways,
@@ -132,10 +146,7 @@ const validate = (code, type) => {
         type = getTypeOfBarcode(modifiedCode);
     }
     // console.warn('* validating ', code, modifiedCode, type);
-    if (validationRegex.exec(modifiedCode) === null) {
-        return false;
-    }
-    const valid = validatorMap[type](modifiedCode);
+    const valid = (type !== 'asin' ? validationRegex.test(modifiedCode) : true) && validatorMap[type](modifiedCode);
     return {
         code,
         type,
@@ -144,7 +155,9 @@ const validate = (code, type) => {
     }
 }
 
-console.warn(validate('092091900451'));
-console.warn(validate('92091900451'));
-console.warn(validate('0083717201410'));
+// console.warn(validate('092091900451')); // valid UPC
+// console.warn(validate('92091900451')); // valid old UPC
+console.warn(validate('0083717201410')); // should be a valid EAN / GTIN13
+console.warn(validate('00837172014104')); // should be a valid GTIN14 !!
+// console.warn(validate('BOOTSTRA P')); // should be an invalid ASIN
 export default validate;
